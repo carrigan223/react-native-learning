8f8fb761d631a3f1db4b06b023891703c1885b25
Andrew Carrigan <carrigan223@gmail.com>
2021-06-26 03:02:14 -0700

    about to start sharing

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import logo from "./assets/logo.png";
+import * as ImagePicker from "expo-image-picker";
 
-export default function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePicker = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("To Use Allow Imagepicker access to your gallery");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.text}>
+        To Share a photo with your friend tap the button below!
+      </Text>
+
+      <TouchableOpacity onPress={openImagePicker} style={styles.button}>
+        <Text style={{ fontSize: 20, color: "black" }}> Pick a photo</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "red",
+    fontSize: 18,
+    textAlign: "center",
+  },
+  logo: {
+    width: 305,
+    height: 159,
+  },
+  button: {
+    backgroundColor: "whitesmoke",
+    borderColor: "black",
+    borderRadius: 13,
+    borderWidth: 2,
+    padding: 15,
+    margin: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
   },
 });
+
+export default App;
